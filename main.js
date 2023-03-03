a3bb6975fac954db1ae7f5f29ffffb81abba1402
Vitalii <vit.vit.00.000@gmail.com>
2023-03-03 19:42:54 +0200

    fix slider

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function openMenu() {
     var header_nav = document.getElementsByClassName('header__nav');
     header_nav[0].classList.toggle('nav__opened');
@@ -17,12 +19,36 @@ var slider = document.getElementsByClassName('hero');
 var currentSlide = 0;
 
 
-function prevSlide() {
-    --currentSlide;
+function changeSlide(direction) {
+    if (direction == "prev") {
+        if (currentSlide !== 0) {
+            currentSlide--;
+        }
+    }
+
+    if (direction == "next") {
+        if (currentSlide !== slider_bg.length - 1) {
+            currentSlide++;
+        }
+    }
+    
     slider[0].style.backgroundImage = 'url(' + slider_bg[currentSlide] + ')';
 }
 
 function nextSlide() {
     ++currentSlide;
     slider[0].style.backgroundImage = 'url(' + slider_bg[currentSlide] + ')';
+}
+
+const links = document.getElementsByClassName('header__link');
+
+for (let link of links) { 
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const anchor = link.getAttribute('href');
+        document.getElementsByClassName(anchor)[0].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
 }
