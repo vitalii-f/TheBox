3e82dc6e7459096bcc6d98fc023dcb2ae099fa75
Vitalii <vit.vit.00.000@gmail.com>
2023-03-03 13:22:57 +0200

    fix header menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function openMenu() {
     var header_nav = document.getElementsByClassName('header__nav');
-    console.log(header_nav[0]);
     header_nav[0].classList.toggle('nav__opened');
-    
+
+    var header_burger = document.getElementsByClassName('header__burger');
+    header_burger[0].classList.toggle('burger__change');   
 }
