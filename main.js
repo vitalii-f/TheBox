ae286470dab794afefb2bb318658b09b07c9a307
Vitalii <vit.vit.00.000@gmail.com>
2023-03-03 14:50:45 +0200

    connect test version of the slider

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ function openMenu() {
     var header_burger = document.getElementsByClassName('header__burger');
     header_burger[0].classList.toggle('burger__change');   
 }
+
+var slider_bg = [
+    '/src/img/hero_bg.jpg',
+    '/src/img/hero_bg_1.webp',
+    '/src/img/hero_bg_2.webp',
+    '/src/img/hero_bg_3.webp',
+]
+
+var slider = document.getElementsByClassName('hero');
+var currentSlide = 0;
+
+
+function prevSlide() {
+    --currentSlide;
+    slider[0].style.backgroundImage = 'url(' + slider_bg[currentSlide] + ')';
+}
+
+function nextSlide() {
+    ++currentSlide;
+    slider[0].style.backgroundImage = 'url(' + slider_bg[currentSlide] + ')';
+}
